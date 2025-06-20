19849f3ed6843ebf53cd264b6c3dad07b420c2de
PriyankaMarne <priyankamarne13@gmail.com>
2025-06-20 23:12:36 +0530

    Initial commit for Personliazed Financial Education application.

---
+++ financialeducation/src/test/java/com/financialeducation/FinancialeducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialeducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialeducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
