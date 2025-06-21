89ba47d6469198e82e44df29ab1bf03ee6fc878e
Suvidha <margajsuvidha@gmail.com>
2025-06-21 18:51:52 +0530

    Added ollama configuration and a test GET api

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # financial-education-api
+
+# install ollama
+1. Download ollama installer from https://ollama.com/download
+2. Install ollama in your local machine
+3. Run ollama model by using command [model chosen for this project is llama3.2:1b] : ollama run llama3.2:1b
+4. Run spring boot application.
+5. Test ollama execute HTTP GET rest call : http://localhost:8080/api/ask?message=%22how%20is%20your%20day%20going%22
--- financialeducation/src/main/java/com/personalizedfinancialeducation/controller/PingController.java
+++ financialeducation/src/main/java/com/personalizedfinancialeducation/controller/PingController.java
@@ -1,13 +1,30 @@
 package com.personalizedfinancialeducation.controller;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
 
+    private final ChatClient chatClient;
+
+    public PingController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
     @GetMapping("/ping")
     public String ping() {
         return "Success";
+    }
+
+    @GetMapping("/ask")
+    public String ask(@RequestParam String message) {
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .content()
+                .toString();
     }
 
 }
